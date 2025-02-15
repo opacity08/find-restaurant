d69387ca59d2b407f9b52f1b6791bbe667b6c9d3
Niko <opacity08@gmail.com>
2025-02-15 22:53:14 +0100

    search bar styles

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   return (
-    <View>
+    <View> 
+      <SearchBar />
       <Text>Search Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+
 
 export default SearchScreen;
