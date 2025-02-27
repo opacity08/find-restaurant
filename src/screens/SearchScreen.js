41c6160214e34ada5a9068b3abf4c20db1a307ef
Niko <mykola@catchmedia.no>
2025-02-27 10:10:07 +0100

    searchBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+
+  const [term, setTerm] = useState('');
+
   return (
     <View> 
-      <SearchBar />
-      <Text>Search Screen</Text>
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
+      <Text>SearchScreen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
