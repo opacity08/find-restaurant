34592a769c45a6d4c57cdb0a525ced84f2990b29
Niko <mykola@catchmedia.no>
2025-04-28 17:00:32 +0200

    Fetching restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 
 const ResultsShowScreen = ({ navigation }) => {
+    const [result, setResult] = useState(null);
     const id = navigation.getParam('id');
 
-    console.log(id);
+    console.log(result);
+
+    const getResults = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResults(id);
+    }, []);
+
+
+    if (!result) {
+        return null;
+    }
 
     return (
         <View>
